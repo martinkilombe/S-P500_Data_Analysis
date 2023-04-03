9a6c6d8dd5bee4d5c55dee611f9f13e6ec7ae9bd
martin kilombe <martinkilombe@gmail.com>
2023-04-03 17:14:05 +0300

    cleand data by removing the commas and full stops

--- S&P 500 employee_ceo data.sql
+++ S&P 500 employee_ceo data.sql
@@ -19,7 +19,7 @@ ALTER TABLE `s&p_500_data`.ceo_data_pay_merged_sp500
 DROP COLUMN pay_ratio;
 #Remove commas(,) and full stops(.) in company_Name field
 UPDATE `s&p_500_data`.ceo_data_pay_merged_sp500
-SET company_name = REPLACE(company_name,',','');
+SET company_name = REPLACE(REPLACE(company_name,',',''),'.','');
 
 #Basic analytics
 #5.Calculating  the highest paid worker and ceo salary from the table
